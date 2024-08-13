653afaab8415d5c7c573a91992b8fc83fd6825a6
OUSee <newerworrying@gmail.com>
2024-08-14 00:59:23 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "" // github base url
-})
+    plugins: [react()],
+    base: "https://github.com/OUSee/SneakersShop", // github base url
+});
