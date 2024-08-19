23c1072fc99d5341aa76c652f7a4670cc60783d1
OUSee <newerworrying@gmail.com>
2024-08-19 16:54:21 +0300

    "Updated FAQSection component with form submission handling and conditional rendering, and made minor styling changes to FAQSection and CartSection components."

--- src/components/FAQSection/FAQSection.tsx
+++ src/components/FAQSection/FAQSection.tsx
@@ -1,32 +1,33 @@
-import { FAQAccordion } from './faqAccordion'
-import styles from './styles.module.css'
+import { FAQAccordion } from "./faqAccordion";
+import styles from "./styles.module.css";
 import instaLogo from "../../assets/instaLogo.png";
-import galaryItem1 from '../../assets/instaGroup/1.png'
+import galaryItem1 from "../../assets/instaGroup/1.png";
 import galaryItem2 from "../../assets/instaGroup/2.png";
 import galaryItem3 from "../../assets/instaGroup/3.png";
 import galaryItem4 from "../../assets/instaGroup/4.png";
 import galaryItem5 from "../../assets/instaGroup/5.png";
+import { useState } from "react";
 
-
-    
 export const FAQSection = () => {
+    const [isformSent, setFormSent] = useState(false);
 
     const handleSubmit = (e: any) => {
-        e.preventDefault()
-        
+        e.preventDefault();
 
         const form = new FormData(e.target);
         const data = Object.fromEntries(form);
-        console.log('Form submitted:', data)
-    }
+        console.log("Form submitted:", data);
+        setFormSent(true);
+    };
 
-    return (
-        <div className={styles.container} onSubmit={handleSubmit}>
-            <div id="faq" className="content">
-                <h1 className={styles.faqTitle}>Часто задаваемые вопросы</h1>
-                <FAQAccordion />
-                <div className={styles.secondSection}>
-                    <form className={styles.askQuestionForm}>
+    const replaceIfhandler = () => {
+        if (!isformSent)
+            return (
+                <>
+                    <form
+                        className={styles.askQuestionForm}
+                        onSubmit={handleSubmit}
+                    >
                         <h2 className={styles.askFormTitle}>
                             Есть еще вопросы?
                         </h2>
@@ -50,10 +51,41 @@ export const FAQSection = () => {
                         <button
                             type="submit"
                             className={styles.askFormSubmitBTN}
-                        >   
+                        >
                             Отправить
                         </button>
                     </form>
+                </>
+            );
+        else {
+            return (
+                <div className={styles.askQuestionForm}>
+                    <h2 className={styles.askFormTitle}>
+                        Спасибо за ваш интерес!
+                    </h2>
+                    <p className={styles.askFormDescription}>
+                        Наш менеджер скоро свяжется с вами
+                    </p>
+                    <div className={styles.gifContainer}>
+                        <img
+                            className={styles.iFrame}
+                            src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3dkcjdjanNoOTVxM3hudjJ3amU1YndueWNpeWphbmEwc2V3bHV5ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT8qBu5gOYEqHhgDQs/giphy.webp"
+                            width="100%"
+                            height="100%"
+                        />
+                    </div>
+                </div>
+            );
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <div id="faq" className="content">
+                <h1 className={styles.faqTitle}>Часто задаваемые вопросы</h1>
+                <FAQAccordion />
+                <div className={styles.secondSection}>
+                    {replaceIfhandler()}
                     <div className={styles.instaGroup}>
                         <img
                             className={styles.instaGrouplogo}
@@ -103,5 +135,4 @@ export const FAQSection = () => {
             </div>
         </div>
     );
-}
-
+};
